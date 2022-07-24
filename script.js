a2c83bf30c0792e39e9459bceae4704206a2f461
vishwanath5jellyfishtech <vishwanatha.naik@jellyfishtechnologies.com>
2022-07-25 01:16:56 +0530

    newupdate

--- script.js
+++ script.js
@@ -60,47 +60,49 @@ function selectFinalStop() {
   //   console.log(val);
 
   let val1 = select1.options[select1.selectedIndex].text;
+  pTag1.textContent = "Your Destination station is: " + val1;
 
-  if (val === val1) {
-    alert("wrong option");
-  } else {
-    pTag1.textContent = "Your Destination station is: " + val1;
-  }
+  // if (val === val1) {
+  //   alert("wrong option");
+  // } else {
+  // }
 }
 
 //------------------------->>>>
 const allFares = {
+  0: 10,
   1: 10,
   2: 10,
   3: 10,
-  4: 10,
+  4: 20,
   5: 20,
   6: 20,
   7: 20,
-  8: 20,
+  8: 30,
   9: 30,
   10: 30,
   11: 30,
-  12: 30,
+  12: 40,
   13: 40,
   14: 40,
   15: 40,
-  16: 40,
+  16: 50,
   17: 50,
   18: 50,
   19: 50,
-  20: 50,
+  20: 60,
   21: 60,
   22: 60,
   23: 60,
-  24: 60,
+  24: 70,
   25: 70,
-  26: 70,
 };
 
 const getPayment = document.getElementById("selectmode");
 const fare = document.getElementById("finalfare");
 ////-------------------------main function
+//source --select
+//distination --select1
 
 function finalFare() {
   const sourceFare = allStops.indexOf(select.value);
@@ -108,21 +110,21 @@ function finalFare() {
   const destinationFare = allStops.indexOf(select1.value);
   // console.log(destinationFare);
   const getFareValue = getPayment.value;
-  console.log(getFareValue);
+  // console.log(getFareValue);
 
   let distance = 0;
-  distance = destinationFare - sourceFare;
 
   if (destinationFare > sourceFare) {
     distance = destinationFare - sourceFare;
   } else if (sourceFare > destinationFare) {
     distance = sourceFare - destinationFare;
+  } else if (destinationFare === sourceFare) {
+    distance = destinationFare;
   } else {
-    return console.log("Select the stations");
+    return alert("Select the stations");
   }
 
   let checkFare = allFares[distance];
-  // console.log(checkFare);
 
   let finalFare = 0;
 
@@ -131,12 +133,16 @@ function finalFare() {
   } else {
     finalFare = checkFare;
   }
+  console.log(finalFare);
 
   if (finalFare < 10) {
     finalFare = 10;
+  } else if (finalFare > 60 && getFareValue === "Card") {
+    finalFare = 60 - (10 / 100) * 60;
   } else if (finalFare > 60) {
     finalFare = 60;
   }
+
   fare.value = finalFare;
 }
 
